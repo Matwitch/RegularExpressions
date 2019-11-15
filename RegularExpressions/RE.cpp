5d49601e97276394bd4194436c2dee4712b8d152
Matthew <matkit27@gmail.com>
2019-11-15 11:26:44 +0200

    gdf

--- RegularExpressions/RE.cpp
+++ RegularExpressions/RE.cpp
@@ -1,5 +1,9 @@
 #include "RE.h"
 #include <stack>
+#include <iostream>
+#include <forward_list>
+
+RE_Graph* RE::nfa = nullptr;
 
 void RE_Graph::Node::add(Node* n)
 {
@@ -25,6 +29,10 @@ void RE_Graph::addEdge(int a, int b)
 	if (a < N && b < N)
 		v[a].add(&v[b]);
 	else throw;
+}
+
+void RE_Graph::DFS(std::forward_list<int>& new_states, std::forward_list<int>& from_states)
+{
 }
 
 void RE_Graph::parse_re(const std::string& reg_expr)
@@ -68,10 +76,37 @@ RE_Graph::RE_Graph(const std::string& reg_expr)
 {
 	N = reg_expr.length() + 1;
 	v = new RE_Graph::Node[N];
+	r = reg_expr;
 	for (int i = 0; i < N - 1; i++)
 		v[i].c = reg_expr.at(i);
 
 	v[N - 1].c = '\0';
 
 	parse_re(reg_expr);
+}
+
+bool RE::recongines(const std::string& re, std::string txt)
+{
+	if (!nfa || !re._Equal(nfa->r))
+	{
+		delete nfa;
+		nfa = new RE_Graph(re);
+	}
+	
+	std::forward_list<int> possible;
+	std::forward_list<int> match;
+	match.push_front(0);
+	nfa->DFS(possible, match);
+
+	for (int i = 0; i < txt.length(); ++i)
+	{
+
+	}
+
+	return true;
+}
+
+bool RE::recongines(const std::string& re, std::istream st)
+{
+	return false;
 }
--- RegularExpressions/RE.h
+++ RegularExpressions/RE.h
@@ -1,6 +1,15 @@
 #pragma once
 #include <string>
 
+namespace std
+{
+	template <class _Ty>
+	class allocator;
+
+	template <class _Ty, class _Alloc = allocator<_Ty>>
+	class forward_list;
+}
+
 class RE_Graph
 {
 private:
@@ -44,10 +53,18 @@ private:
 public:
 	Node* v;
 	int N;
-
+	std::string r;
 	void addEdge(int a, int b);
-
+	void DFS(std::forward_list<int>& new_states, std::forward_list<int>& from_states);
 	void parse_re(const std::string& reg_expr);
 
 	RE_Graph(const std::string& reg_expr);
+};
+
+
+class RE
+{
+	static RE_Graph* nfa;
+	static bool recongines(const std::string& re, std::string txt);
+	static bool recongines(const std::string& re, std::istream st);
 };
